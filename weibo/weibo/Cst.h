1747560cd82e7029d102387cec004fac6361ab49
zs <zs@163.com>
2016-05-02 22:00:05 +0800

    创建cat

---
+++ weibo/weibo/Cst.h
@@ -0,0 +1,13 @@
+//
+//  Cst.h
+//  weibo
+//
+//  Created by chushiyang on 16/5/2.
+//  Copyright © 2016年 chushiyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cst : NSObject
+
+@end
