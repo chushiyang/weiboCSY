9c425f34f8119e1fc0718272d4fffe87680ad460
zs <zs@163.com>
2016-05-02 21:56:30 +0800

    初始化项目

---
+++ weibo/weibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weibo
+//
+//  Created by chushiyang on 16/5/2.
+//  Copyright © 2016年 chushiyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
